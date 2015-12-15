7616d0cd537e47dc1a057e407e032d3775d66a63
alumno <alumno@U109021>
2015-12-15 18:54:33 +0100

    primer intento del ejemplo, ha costado un poco pero a molado

---
+++ ejemplo1/app/src/main/java/com/example/alumno/ejemplo1/MenuActivity.java
@@ -0,0 +1,35 @@
+package com.example.alumno.ejemplo1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        TextView textLogin = (TextView)findViewById(R.id.menu_login);
+        textLogin.setText(intent.getStringExtra(MainActivity.EXTRA_LOGIN));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
